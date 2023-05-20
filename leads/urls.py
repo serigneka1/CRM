d2351b148d7e952f6f8898fce5dd52fd2c4fe338
serigneka1 <kaserigne69@gmail.com>
2023-05-20 18:31:13 +0200

    Add lead, Edit lead, Delete Lead

--- leads/urls.py
+++ leads/urls.py
@@ -5,7 +5,9 @@ app_name = 'leads'
 urlpatterns=[
 
     path("", views.leads_list, name="leads_list"),
-    path("add_lead/", views.add_lead, name="add_lead"),
     path("<int:pk>/", views.leads_detail, name="leads_detail"),
+    path('<int:pk>/delete/', views.leads_delete, name='leads_delete'),
+    path("add_lead/", views.add_lead, name="add_lead"),
+    path('<int:pk>/edit/', views.leads_edit, name="leads_edit"),
 
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . forms import AddLeadForm
 from .models import Lead
 
@@ -9,25 +10,52 @@ def leads_list(request):
     leads= Lead.objects.filter(created_by=request.user)
     return render(request, 'leads_list.html', {'leads':leads})
 
+
+@login_required
+def leads_detail(request, pk):
+    lead=get_object_or_404(Lead, created_by=request.user, pk=pk)
+
+    return render(request, 'leads_detail.html',{ 'lead':lead})
+
+
+
+@login_required
+def leads_delete(request, pk):
+    lead=get_object_or_404(Lead, created_by=request.user, pk=pk)
+    lead.delete()
+    messages.success(request, 'vous avez supprimé le prospect')
+    return redirect('leads:leads_list')
+
+@login_required
+def leads_edit(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Les changement ont été effectués')
+            return redirect('leads:leads_list')
+
+    else:
+        form=AddLeadForm(instance=lead)
+        return render(request, 'leads_edit.html', {'form': form})
+
+
+
+
 @login_required
 def add_lead(request):
     if request.method=='POST':
         form=AddLeadForm(request.POST)
-
         if form.is_valid():
             lead=form.save(commit=False)
             lead.created_by=request.user
             lead.save()
-
-            return redirect('dashboard:dashboard')
+            messages.success(request, 'Un prospect a été ajouté')
+            return redirect('leads:leads_list')
     else:
         form= AddLeadForm()
 
     return render(request, 'add_lead.html', {'form':form})
 
-@login_required
-def leads_detail(request, pk):
-    lead=Lead.objects.filter(created_by=request.user).get(pk=pk)
-
-    return render(request, 'leads_detail.html',{ 'lead':lead})
 
